684a4ee499cf94bb9b8e44a20d2cc6f1d1492451
aubreyrui <157694302+aubreyrui@users.noreply.github.com>
2024-02-26 14:25:46 +0800

    02-26-24 Update

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import TaskGroup, Task
 
 # Register your models here.
+
+class TaskGroupAdmin(admin.ModelAdmin):
+    model = TaskGroup
+
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+
+    list_display = ['name', 'due_date']
+
+admin.site.register(TaskGroup, TaskGroupAdmin)
+admin.site.register(Task, TaskAdmin)
